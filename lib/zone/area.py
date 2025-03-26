4d9d7ff418f4e455ce365f6c3b1ca76795d3dc8f
许伟旗 <xuweiqizj@163.com>
2025-03-26 15:49:07 +0800

    更新

--- lib/zone/area.py
+++ lib/zone/area.py
@@ -35,7 +35,6 @@ def get_areas(city, district):
         html = response.content
         root = etree.HTML(html)
         links = root.xpath(DISTRICT_AREA_XPATH)
-        print(links)
 
         # 针对a标签的list进行处理
         for link in links:
